44dbb28d39b2433fc7f0e526dfd3fa30b79becbd
Chinonso Peace Mbama <mbamachinonso@gmail.com>
2022-10-15 20:01:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const beat = document.querySelector('.box');
+const code = document.querySelector('.box h1');
+const sound = document.querySelector('.box p')
+
+
+beat.addEventListener('click', (e) => {
+    
+
+})
